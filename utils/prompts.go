f00b14114f7a40c505052c75bd4af03ac75e0fb3
Omar Rafat <omarrafat60@gmail.com>
2023-08-14 23:25:30 +0300

    Added accepting dbConfig from flags. If provided, the user won't be prompted.

--- utils/prompts.go
+++ utils/prompts.go
@@ -11,126 +11,134 @@ import (
 
 // PromptForDbConfig prompts the user for their database configuration.
 // It returns a DbConfig and an error.
-func PromptForDbConfig() (types.DbConfig, safego.Option[error]) {
+func PromptForDbConfig(dbConfig *types.DbConfig) safego.Option[error] {
 	// Dialect
-	dialectPrmpt := promptui.Select{
-		Label: "Dialect",
-		Items: vars.SupportedDatabases,
-	}
-	_, dialect, err := dialectPrmpt.Run()
-	if err != nil {
-		defaultVal := types.DbConfig{}
-		return defaultVal, safego.Some(err)
+	if dbConfig.Dialect == "" {
+		dialectPrmpt := promptui.Select{
+			Label: "Dialect",
+			Items: vars.SupportedDatabases,
+		}
+		_, dialect, err := dialectPrmpt.Run()
+		if err != nil {
+			return safego.Some(err)
+		}
+
+		dbConfig.Dialect = dialect
 	}
 
 	// Host
-	HostPrmpt := promptui.Prompt{
-		Label: "Host",
-		Validate: func(s string) error {
-			if s == "" {
-				return fmt.Errorf("host cannot be empty")
-			}
+	if dbConfig.Host == "" {
+		HostPrmpt := promptui.Prompt{
+			Label: "Host",
+			Validate: func(s string) error {
+				if s == "" {
+					return fmt.Errorf("host cannot be empty")
+				}
 
-			return nil
-		},
-	}
-	host, err := HostPrmpt.Run()
-	if err != nil {
-		defaultVal := types.DbConfig{}
-		return defaultVal, safego.Some(err)
+				return nil
+			},
+		}
+		host, err := HostPrmpt.Run()
+		if err != nil {
+			return safego.Some(err)
+		}
+
+		dbConfig.Host = host
 	}
 
 	// Port
-	PortPrmpt := promptui.Prompt{
-		Label: "Port",
-		Validate: func(s string) error {
-			if s == "" {
-				return fmt.Errorf("port cannot be empty")
-			}
-			// Check if the port is a number.
-			intPort, err := strconv.Atoi(s)
-			if err != nil {
-				return fmt.Errorf("port must be a number")
-			}
+	if dbConfig.Port == 0 {
+		PortPrmpt := promptui.Prompt{
+			Label: "Port",
+			Validate: func(s string) error {
+				if s == "" {
+					return fmt.Errorf("port cannot be empty")
+				}
+				// Check if the port is a number.
+				intPort, err := strconv.Atoi(s)
+				if err != nil {
+					return fmt.Errorf("port must be a number")
+				}
 
-			if intPort < 0 || intPort > 65535 {
-				return fmt.Errorf("port must be between 0 and 65535")
-			}
+				if intPort < 0 || intPort > 65535 {
+					return fmt.Errorf("port must be between 0 and 65535")
+				}
 
-			return nil
-		},
-	}
-	stringPort, err := PortPrmpt.Run()
-	if err != nil {
-		defaultVal := types.DbConfig{}
-		return defaultVal, safego.Some(err)
-	}
-	port, err := strconv.Atoi(stringPort)
-	if err != nil {
-		defaultVal := types.DbConfig{}
-		return defaultVal, safego.Some(err)
+				return nil
+			},
+		}
+		stringPort, err := PortPrmpt.Run()
+		if err != nil {
+			return safego.Some(err)
+		}
+		port, err := strconv.Atoi(stringPort)
+		if err != nil {
+			return safego.Some(err)
+		}
+
+		dbConfig.Port = port
 	}
 
 	// User
-	userPrmpt := promptui.Prompt{
-		Label: "User",
-		Validate: func(s string) error {
-			if s == "" {
-				return fmt.Errorf("user cannot be empty")
-			}
+	if dbConfig.Username == "" {
+		userPrmpt := promptui.Prompt{
+			Label: "User",
+			Validate: func(s string) error {
+				if s == "" {
+					return fmt.Errorf("user cannot be empty")
+				}
 
-			return nil
-		},
-	}
-	user, err := userPrmpt.Run()
-	if err != nil {
-		defaultVal := types.DbConfig{}
-		return defaultVal, safego.Some(err)
+				return nil
+			},
+		}
+		user, err := userPrmpt.Run()
+		if err != nil {
+			return safego.Some(err)
+		}
+
+		dbConfig.Username = user
 	}
 
 	// Password
-	passwordPrmpt := promptui.Prompt{
-		Label: "Password",
-		Mask:  '*',
-		Validate: func(s string) error {
-			if s == "" {
-				return fmt.Errorf("password cannot be empty")
-			}
+	if dbConfig.Password == "" {
+		passwordPrmpt := promptui.Prompt{
+			Label: "Password",
+			Mask:  '*',
+			Validate: func(s string) error {
+				if s == "" {
+					return fmt.Errorf("password cannot be empty")
+				}
 
-			return nil
-		},
-	}
-	password, err := passwordPrmpt.Run()
-	if err != nil {
-		defaultVal := types.DbConfig{}
-		return defaultVal, safego.Some(err)
+				return nil
+			},
+		}
+		password, err := passwordPrmpt.Run()
+		if err != nil {
+			return safego.Some(err)
+		}
+
+		dbConfig.Password = password
 	}
 
 	// Database name
-	databasePrmpt := promptui.Prompt{
-		Label: "Database name",
-		Validate: func(s string) error {
-			if s == "" {
-				return fmt.Errorf("database name cannot be empty")
-			}
+	if dbConfig.Database == "" {
+		databasePrmpt := promptui.Prompt{
+			Label: "Database name",
+			Validate: func(s string) error {
+				if s == "" {
+					return fmt.Errorf("database name cannot be empty")
+				}
 
-			return nil
-		},
-	}
-	database, err := databasePrmpt.Run()
-	if err != nil {
-		defaultVal := types.DbConfig{}
-		return defaultVal, safego.Some(err)
-	}
+				return nil
+			},
+		}
+		databaseName, err := databasePrmpt.Run()
+		if err != nil {
+			return safego.Some(err)
+		}
 
-	dbConfig := types.DbConfig{
-		Dialect:  dialect,
-		Host:     host,
-		Port:     port,
-		Username: user,
-		Password: password,
-		Database: database,
+		dbConfig.Database = databaseName
 	}
 
-	return dbConfig, safego.None[error]()
+	return safego.None[error]()
 }
